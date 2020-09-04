e1fc7454e63c6e394fa3bf1f0533711cf3fad9fb
VictorGazzinelli <vpezzigc@gmail.com>
2020-09-04 13:29:11 -0300

    feat: Add geolocation lat lng

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
-function helloWorld() {
-  for (let i = 3; i > 0; i-- ) {
-  console.log(i);
+window.addEventListener('load', getLocation);
+
+let geolocationInput = null
+
+async function getLocation() {
+  geolocationInput = document.querySelector('#geolocationInput')
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(onGetCurrentPositionSuccess, onGetCurrentPositionError);
+  } else {
+    geolocationInput.innerHTML = "Não foi possível utilizar a API de geolocalização no seu browser atual";
   }
-  console.log('...');
-  console.log('Hello World!');
- }
- helloWorld();
+}
+
+function onGetCurrentPositionSuccess(position) {
+  geolocationInput.innerHTML = `Latitude: ${position.coords.latitude} Longitude: ${position.coords.longitude}`;
+}
+
+function onGetCurrentPositionError(error){
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      geolocationInput.innerHTML = "Você negou acesso a sua localização"
+      break;
+    case error.POSITION_UNAVAILABLE:
+      geolocationInput.innerHTML = "Sua localização está indisponível"
+      break;
+    case error.TIMEOUT:
+      geolocationInput.innerHTML = "Tempo de requisição expirou"
+      break;
+    case error.UNKNOWN_ERROR:
+      geolocationInput.innerHTML = "Um erro desconhecido ocorreu"
+      break;
+  }
+}
